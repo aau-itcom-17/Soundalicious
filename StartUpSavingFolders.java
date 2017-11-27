41d156377b075ab6ebde818ef3499011d50ed27e
akroghp <32833655+akroghp@users.noreply.github.com>
2017-11-27 11:24:14 +0100

    Add files via upload

--- StartUpSavingFolders.java
+++ StartUpSavingFolders.java
@@ -18,7 +18,7 @@ public class StartUpSavingFolders {
         File soundFolder = new File(homePath + File.separator + "p1" + File.separator + "Sounds");
         if (appFolder.exists() && appFolder.isDirectory()) {
             System.out.println("app folder: " + appFolder);
-            Questions questions = new Questions();
+            //Questions questions = new Questions(); Here we need to import the xml page.
         } else {
             if (appFolder.mkdir()) {
                 System.out.println("P1 folder was created....");
@@ -40,11 +40,21 @@ public class StartUpSavingFolders {
         // copy the sounds folder to the computer
 
         String k = "";
-        for (int i  = 15; i > 0; i--) {
+        String k1 = "";
+        for (int i  = 10; i > 0; i--) {
 
             try {
 
                 File soundPackageFile = new File(homePath + File.separator + "downloads" + File.separator + "P1-master" + k + File.separator + "Sounds");
+                File soundPackageFileWithSlash = new File(homePath + File.separator + "downloads" + File.separator + "P1-master" + k1 + File.separator + "Sounds");
+                File soundPackageFileWithSlashIdea = new File(homePath + File.separator + "IdeaProjects" + File.separator + "P1-master" + k1 + File.separator + "Sounds");
+
+                if (!soundPackageFile.exists()){
+                    soundPackageFile = soundPackageFileWithSlash;
+                }
+                if (!soundPackageFileWithSlash.exists()){
+                    soundPackageFile = soundPackageFileWithSlashIdea;
+                }
 
                 System.out.println("File path to sound folder in downloads: " + soundPackageFile);
 
@@ -55,11 +65,11 @@ public class StartUpSavingFolders {
                 System.out.println("Size of sound folder from github: "+f1);
                 System.out.println("Size of the user folder: "+f2);
 
-            if (f1 != f2) {
-                // copies the folder on startup
-                CopyFolder.copyFolder(soundPackageFile, soundFolder);
-                System.out.println("Folder from github has been copied to this computer");
-            }
+                if (f1 != f2) {
+                    // copies the folder on startup
+                    CopyFolder.copyFolder(soundPackageFile, soundFolder);
+                    System.out.println("Folder from github has been copied to this computer");
+                }
                 if (soundPackageFile.exists()){
                     break;
                 }
@@ -69,6 +79,7 @@ public class StartUpSavingFolders {
             }
 
             k = " " + i + "";
+            k1 = "-" + i + "";
         }
 
     }
