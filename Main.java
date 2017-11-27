b5b8f42699c0e5d00644ae281e942a8867d3bf6b
Bastian Højbjerre <bastianhoejbjerre@gmail.com>
2017-11-27 09:54:49 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -33,6 +33,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
 
+      StartUpSavingFolders.startUpSavingFolders();
       window = primaryStage;
       new FrontPageScene();
       window.setTitle("Soundalicous");
