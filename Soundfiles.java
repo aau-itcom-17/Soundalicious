c11a1e262b062738f51b65c730fa01ecfc447a7b
akroghp <32833655+akroghp@users.noreply.github.com>
2017-11-30 14:08:36 +0100

    Add files via upload

--- Soundfiles.java
+++ Soundfiles.java
@@ -9,177 +9,42 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
-public class Soundfiles
-{
+public class Soundfiles {
     private static int count = 0;
-    
+
     private static AudioInputStream audio;
     private static Clip clip = null;
-// Makes the sound stop. 
+
+    // Makes the sound stop.
     public static void noSound() {
         clip.stop();
     }
 
-// Only this method has been changed to clip. The other methods remain.
-    public static void kanyeSound() {
+    // Only this method has been changed to clip. The other methods remain.
+    public static void readingSounds() {
         //Play sound
         try {
-             String homePath = System.getProperty("user.home");
+            String homePath = System.getProperty("user.home");
+            //reading files from the path of in the program folder on the computer. Path:
             File soundFolder = new File(homePath + File.separator + "p1" + File.separator + "Sounds" + File.separator + Main.rQuestions.get(0).getSoundfile());
             String soundFolderName = soundFolder.getPath();
+            System.out.println(soundFolderName);
+
+            // Playing soundfile from file
             audio = AudioSystem.getAudioInputStream(new File(soundFolderName));
             clip = AudioSystem.getClip();
             clip.open(audio);
             clip.start();
-            
-        }
-        
-        
-        catch(UnsupportedAudioFileException uae) {
+
+        } catch (UnsupportedAudioFileException uae) {
             System.out.println(uae);
-        }
-        catch(IOException ioe) {
+        } catch (IOException ioe) {
             System.out.println(ioe);
-        }
-        catch(LineUnavailableException lua) {
+        } catch (LineUnavailableException lua) {
             System.out.println(lua);
         }
 
 
     }
-
-    
-/*    public static void kanyeSound()
-    {
-
-   
-
-    private static AudioInputStream audio;
-    private static Clip clip = null;
-// Makes the sound stop.
-    public static void noSound() {
-        clip.stop();
-    }
-
-// Only this method has been changed to clip. The other methods remain.
-    public static void kanyeSound() {
-        //Play sound
-        try {
-            audio = AudioSystem.getAudioInputStream(new File("/Users/Nius/IdeaProjects/P1/src/K.au"));
-            clip = AudioSystem.getClip();
-            clip.open(audio);
-            clip.start();
-            
-        }
-        
-        
-        catch(UnsupportedAudioFileException uae) {
-            System.out.println(uae);
-        }
-        catch(IOException ioe) {
-            System.out.println(ioe);
-        }
-        catch(LineUnavailableException lua) {
-            System.out.println(lua);
-        }
-
-
-    }
-
-        
-        
-/*       
-        //Play sound
-        try
-        {
-            String gongFile = "K.au";
-
-            InputStream in = new FileInputStream(gongFile);
-
-            // create an audiostream from the inputstream
-            AudioStream audioStream = new AudioStream(in);
-
-            // play the audio clip with the audioplayer class
-            AudioPlayer.player.start(audioStream);
-
-        }
-        catch (IOException exception)
-        {
-            exception.printStackTrace();
-        }
-
-    }
-*/
-
-    public static void darudeSound()
-    {
-        //Play sound
-        try
-        {
-            String gongFile = "K.au";
-
-            InputStream in = new FileInputStream(gongFile);
-
-            // create an audiostream from the inputstream
-            AudioStream audioStream = new AudioStream(in);
-
-            // play the audio clip with the audioplayer class
-            AudioPlayer.player.start(audioStream);
-
-        }
-        catch (IOException exception)
-        {
-            exception.printStackTrace();
-        }
-    }
-
-    public static void bMarsSound()
-    {
-        //Play sound
-        try
-        {
-            String gongFile = "K.au";
-
-            InputStream in = new FileInputStream(gongFile);
-
-            // create an audiostream from the inputstream
-            AudioStream audioStream = new AudioStream(in);
-
-            // play the audio clip with the audioplayer class
-            AudioPlayer.player.start(audioStream);
-
-        }
-        catch (IOException exception)
-        {
-            exception.printStackTrace();
-        }
-    }
-
-    public static void countPoints()
-    {
-        count++;
-
-        Alert alert = new Alert(Alert.AlertType.INFORMATION);
-        alert.setTitle("Information Dialog");
-        alert.setHeaderText(null);
-        alert.setContentText("Correct answer!\n" + "You have " + count + " correct answers");
-
-        alert.showAndWait();
-    }
-
-    public static void resetCountPoints()
-    {
-        count = 0;
-    }
-
-    public static String getChoice(ChoiceBox<String> choiceBox2)
-    {
-        String questions = choiceBox2.getValue();
-        return questions;
-    }
-
 }
-
-
-
 
