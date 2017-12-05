36017d36a620c3ba4bdf8a8ec10c144f56a8b260
Lasse Skjøtt Hansen <32833696+LasseSkjott@users.noreply.github.com>
2017-12-05 11:19:47 +0100

    Update ScoreboardPageScene.java

--- ScoreboardPageScene.java
+++ ScoreboardPageScene.java
@@ -90,6 +90,10 @@ public class ScoreboardPageScene extends Main{
         });
 
         finGame = new Button("Finish game");
+        teams.clear();
+        n = 0;
+        rQuestions.clear();
+        answers.clear();
         finGame.setOnAction(e -> window.setScene(frontPageScene));
 
         layoutScoreboard = new VBox(20);
